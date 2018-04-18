526b51fdbe54d6a7e52de1fb0fe8a5c88e4247d0
david <david.ravel@yahoo.fr>
2018-04-18 18:52:17 +0200

    Création du fichier modele.php

---
+++ MVC/modele.php
@@ -0,0 +1,17 @@
+<?php
+function getBillets()
+{
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=test;chartset=utf8', 'root', '');
+	}
+	catch(Exeption $e)
+	{
+		die('Erreur : '.$e->getMessag());
+	}
+
+	$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh %imin %ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+	
+	return $req;
+}
+?>
