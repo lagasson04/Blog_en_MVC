8270343a5b8c148c007af436ab4d1433bd5a5b88
david <david.ravel@yahoo.fr>
2018-04-18 19:41:01 +0200

    Modification modele.php

--- MVC/modele.php
+++ MVC/modele.php
@@ -1,5 +1,5 @@
 <?php
-function getBillets()
+/*function getBillets()
 {
 	try
 	{
@@ -7,12 +7,28 @@ function getBillets()
 	}
 	catch(Exeption $e)
 	{
-		die('Erreur : '.$e->getMessag());
+		die('Erreur : '.$e->getMessage());
 	}
 
-	$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh %imin %ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
-	
+	$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+
+	return $req;
+}*/
+?>
+<?php
+function getBillets()
+{
+	try
+	{
+	    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+	}
+	catch(Exception $e)
+	{
+	    die('Erreur : '.$e->getMessage());
+	}
+
+	$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+
 	return $req;
 }
 ?>
-<!--Création de la fonction qui renvoie la liste des billets-->
