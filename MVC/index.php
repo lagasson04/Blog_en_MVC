b6aa4d52f6883440b21435a1241a5ef0686460ca
david <david.ravel@yahoo.fr>
2018-04-18 18:29:31 +0200

    Premiere refactorisation, séparation de la partie qui collecte les données de celle qui les affichent

--- MVC/index.php
+++ MVC/index.php
@@ -1,3 +1,21 @@
+<?php  
+// Récupération des données
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '' );
+}
+catch(Exeption $e)
+{
+	die('Erreur : '.$e->getMessage());
+}
+
+$req = $bdd->query('SELECT id, titre,contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh %imin %ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+?>
+
+
+<!-- Affichage -->
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,20 +28,8 @@
 	<h1>Bienvenue sur mon blog</h1>
 	<p>Derniers billets du blog :</p>
 
+	
 	<?php
-// Connexion à la base de données
-	try
-	{
-		$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
-	}
-	catch(Exception $e)
-	{
-		die('Erreur : '.$e->getMessage());
-	}
-
-// On récupère les 5 derniers billets
-	$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \' %d/%m/%Y à %Hh %imin %ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
-
 	while ($donnees = $req->fetch()) 
 	{
 		?>
