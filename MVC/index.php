782dd3ab1b7c733ead9e64c938e5f1bf82adc540
david <david.ravel@yahoo.fr>
2018-04-18 19:42:03 +0200

    Modification fichier index.php

--- MVC/index.php
+++ MVC/index.php
@@ -1,14 +1,7 @@
-<?php  
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '' );
-}
-catch(Exeption $e)
-{
-	die('Erreur : '.$e->getMessage());
-}
+<?php
+require('modele.php');
 
-$req = $bdd->query('SELECT id, titre,contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh %imin %ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+$req = getBillets();
 
 require('affichageAccueil.php');
 ?>
