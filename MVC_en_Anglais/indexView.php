2bae1714e00f7e0029909e067fb32029a139ed65
david <david.ravel@yahoo.fr>
2018-04-19 09:30:03 +0200

    Ajout du fichier indexView.php

---
+++ MVC_en_Anglais/indexView.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Mon super blog</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<h1>Bienvenue sur mon blog</h1>
+	<p>Derniers billets du blog :</p>
+
+	
+	<?php
+	while ($data = $req->fetch()) 
+	{
+	?>
+		<div class="news">
+			<h3>
+				<?= htmlspecialchars($data['title']) ?>
+				<em>le <?= $data['creation_date_fr'] ?></em>
+			</h3>
+
+			<p>
+				<?php
+				echo nl2br(htmlspecialchars($data['content']));
+				?>
+				<br />
+				<em><a href="#">Commentaires</a></em>
+			</p>
+		</div>
+	<?php
+	}
+	$req->closeCursor();
+	?>
+</body>
+</html>
