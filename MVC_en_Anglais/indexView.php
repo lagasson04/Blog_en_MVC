ecfcc97b1ee4e0c77661074dd9e8a7bee8e6d516
david <david.ravel@yahoo.fr>
2018-04-24 09:54:13 +0200

    Modification du fichier indexView.php

--- MVC_en_Anglais/indexView.php
+++ MVC_en_Anglais/indexView.php
@@ -1,37 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Mon super blog en MVC</title>
-	<link rel="stylesheet" href="style.css">
-</head>
+<?php $title = 'Mon super blog en MVC'; ?>
 
-<body>
-	<h1>Bienvenue sur mon blog</h1>
-	<p>Derniers billets du blog :</p>
+<?php ob_start(); ?>
+<h1>Bienvenue sur mon blog</h1>
+<p>Derniers billets du blog :</p>
 
-	
-	<?php
-	while ($data = $req->fetch()) 
-	{
+
+<?php
+while ($data = $req->fetch()) 
+{
 	?>
-		<div class="news">
-			<h3>
-				<?= htmlspecialchars($data['title']) ?>
-				<em>le <?= $data['creation_date_fr'] ?></em>
-			</h3>
+	<div class="news">
+		<h3>
+			<?= htmlspecialchars($data['title']) ?>
+			<em>le <?= $data['creation_date_fr'] ?></em>
+		</h3>
 
-			<p>
-				<?php
-				echo nl2br(htmlspecialchars($data['content']));
-				?>
-				<br />
-				<em><a href="post.php?id=<?=$data['id'] ?>">Commentaires</a></em>
-			</p>
-		</div>
+		<p>
+			<?php
+			echo nl2br(htmlspecialchars($data['content']));
+			?>
+			<br />
+			<em><a href="post.php?id=<?=$data['id'] ?>">Commentaires</a></em>
+		</p>
+	</div>
 	<?php
-	}
-	$req->closeCursor();
-	?>
-</body>
-</html>
+}
+$req->closeCursor();
+?>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
+
