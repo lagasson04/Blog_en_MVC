5fe8cd43c1638d2beda9e87b5800e46db01d1a69
david <david.ravel@yahoo.fr>
2018-04-24 16:00:58 +0200

    Modification des fichiers pour l'ajout de la fonctionnalité ajout de commentaires

--- project/model/frontend.php
+++ project/model/frontend.php
@@ -2,7 +2,7 @@
 function getPosts()
 {
     $db = dbConnect();
-    $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+    $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh %imin %ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
 
     return $req;
 }
@@ -10,7 +10,7 @@ function getPosts()
 function getPost($postId)
 {
     $db = dbConnect();
-    $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+    $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh %imin %ss\') AS creation_date_fr FROM posts WHERE id = ?');
     $req->execute(array($postId));
     $post = $req->fetch();
 
@@ -20,7 +20,7 @@ function getPost($postId)
 function getComments($postId)
 {
     $db = dbConnect();
-    $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+    $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh %imin %ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
     $comments->execute(array($postId));
 
     return $comments;
@@ -37,4 +37,12 @@ function dbConnect()
     {
         die('Erreur : '.$e->getMessage());
     }
+}
+
+function postComment($postId, $author, $comment)
+{
+    $db = dbConnect();
+    $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES (?, ?, ?, NOW())');
+    $affectedLines = $comments->execute(array($postId, $author, $comment));
+    return $affectedLines;
 }
