5306d165684db2e34d26127449b62fe992f3ac90
david <david.ravel@yahoo.fr>
2018-04-26 10:36:01 +0200

    Ajout du fichier updateCommentView.php

---
+++ Blog_en_POO/view/frontend/updateCommentView.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Mon blog</title>
+	<link href="public/css/style.css" rel="stylesheet" /> 
+</head>
+<body>
+	<p>Ici, vous pouvez modifier les commentaires :</p>
+	<form action="#" method="post">
+		<div>
+			<label for="author">Auteur</label><br />
+			<input type="text" id="author" name="author" value="<?= htmlspecialchars($_GET['auteur']) ?>" required="required"/>
+		</div>
+		<div>
+			<label for="comment">Commentaire</label><br />
+			<textarea id="comment" name="comment" ><?= htmlspecialchars($_GET['com']) ?></textarea>
+		</div>
+		<div>
+			<input type="submit" />
+		</div>
+	</form>
+	<p><a href="../../index.php">Retour à l'accueil</a></p>
+</body>
+</html>
+
+
